3c6948b2b2d417d738c17b31d2ef43a4cf7d0b8f
Nguyen The Phuong <thephuongcqt@gmail.com>
2018-05-24 15:30:17 +0700

    Bug return xml

--- Controller/TwilioController.js
+++ Controller/TwilioController.js
@@ -2,6 +2,7 @@ module.exports = function(app, express){
     var apiRouter = express.Router();
 
     apiRouter.get("/Voice", function(req, res){
+        res.set('Content-Type', 'text/xml');
         const VoiceResponse = require('twilio').twiml.VoiceResponse;
         const response = new VoiceResponse();
         response.play('https://firebasestorage.googleapis.com/v0/b/chatfirebase-1e377.appspot.com/o/Welcome.mp3?alt=media&token=6914df70-85d3-4ea4-9ce0-edf4516ea353');
