8d0b1fe30dd3038216d0e6399c43f1f0391469c4
duyncse61032 <duyncse61032@fpt.edu.vn>
2018-05-31 15:37:43 +0700

    update function for website

--- server.js
+++ server.js
@@ -3,7 +3,25 @@ var app = express();
 var userController = require("./Controller/UserController")(app, express);
 var userDB = require("./Controller/UserDBController");
 var twilioController = require("./Controller/TwilioController")(app, express);
+// Add headers
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 /////////////////////////////////////////////////////////////////////////
 // require to UsersController
 var usersController = require("./Controller/UsersController");
