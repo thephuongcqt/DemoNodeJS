ac68fb0bff61a332b8dc9e1cbae4b62f0951102f
Nguyen The Phuong <thephuongcqt@gmail.com>
2018-06-02 11:12:05 +0700

    Edit recordingStatusCallback URL

--- Controller/TwilioController.js
+++ Controller/TwilioController.js
@@ -9,10 +9,11 @@ module.exports = function(app, express){
     apiRouter.get("/Voice", function(req, res){
         res.set('Content-Type', 'text/xml');        
         const VoiceResponse = require('twilio').twiml.VoiceResponse;
+        var recordURL = req.protocol + '://' + req.get('host') + '/record';
         const twiml = new VoiceResponse();
         twiml.play('https://firebasestorage.googleapis.com/v0/b/chatfirebase-1e377.appspot.com/o/Welcome.mp3?alt=media&token=6914df70-85d3-4ea4-9ce0-edf4516ea353');
         twiml.record({
-            recordingStatusCallback: 'https://callcenterapi.herokuapp.com/twilio/Recorded',
+            recordingStatusCallback: recordURL,
             method: 'POST',
         });
         res.end(twiml.toString());        
