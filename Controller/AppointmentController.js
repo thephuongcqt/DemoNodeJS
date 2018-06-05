7f862fc9912bc6c1fd0c3a7b65b47e588a864efd
duyncse61032 <duyncse61032@fpt.edu.vn>
2018-06-05 15:47:19 +0700

    update appointment

--- Controller/AppointmentController.js
+++ Controller/AppointmentController.js
@@ -2,61 +2,33 @@ var db = require("../Utils/DBUtils");
 var utils = require("../Utils/Utils");
 var Const = require("../Utils/Const");
 
-module.exports = function(app, express){
+module.exports = function (app, express) {
     apiRouter = express.Router();
 
-    apiRouter.get("/getAll", function(req, res){
+    apiRouter.get("/getAll", function (req, res) {
         db.Appointment.forge()
-        .fetchAll()
-        .then(function(collection){
-            var responseObj = utils.makeResponse(true, collection.toJSON(), false);
-            res.json(responseObj);
-        })
-        .catch(function(err){
-            var responseObj = utils.makeResponse(false, err.message, true);
-            res.json(responseObj);            
-        });
+            .fetchAll()
+            .then(function (collection) {
+                var responseObj = utils.makeResponse(true, collection.toJSON(), false);
+                res.json(responseObj);
+            })
+            .catch(function (err) {
+                var responseObj = utils.makeResponse(false, err.message, true);
+                res.json(responseObj);
+            });
     });
 
-    apiRouter.get("/findAppointmentForClinic", function(req, res){
-        var clinicUsername = req.query.clinicUsername;
-        var sql = "SELECT * FROM tbl_appointment WHERE clinicUsername = '" + clinicUsername + "' AND DATE(appointmentTime) = CURRENT_DATE()";        
-        db.knex.raw(sql)
-        .then(function(collection){
-            result = collection[0];                
-            if(result.length > 0){  
-                var tmp = [];
-                for(var i in result){        
-                    tmp.push(result[i].patientID);
-                }
-                db.Patient.forge()
-                .where("patientID", "in", tmp)
-                .fetchAll()
-                .then(function(patientsResult){
-                    var results = [];
-                    for(var i in result){
-                        var tmpAppointment = JSON.parse(JSON.stringify(result[i]));
-                        // console.log(tmpAppointment);
-                        for(j in patientsResult.models){                                                        
-                            var tmpPatient = patientsResult.models[j].toJSON();                                                        
-                            if(tmpAppointment.patientID == tmpPatient.patientID){                            
-                                tmpAppointment.patient = tmpPatient;                                
-                            }
-                        }   
-                        delete tmpAppointment.clinicUsername;
-                        delete tmpAppointment.patientID;
-                        results.push(tmpAppointment);
-                    }
-                    res.json(utils.makeResponse(true, results, null));
-                });                
-            } else{
-                res.json(utils.makeResponse(false, null, "Không có cuộc hẹn nào"));
-            }                      
-        })
-        .catch(function(err){
-            var responseObj = utils.makeResponse(false, null, err.message);
-            res.json(responseObj);            
-        });
+    apiRouter.get("/findAppointmentForClinic", function (req, res) {
+        var username = req.query.username;
+        new db.Clinic({ "username": username })
+            .fetch({ withRelated: ["appointments"] })
+            .then(function (model) {
+                var clinic = model.toJSON();
+                res.json(utils.makeResponse(true, clinic, null));
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
     });
 
     return apiRouter;
--- Controller/ClinicController.js
+++ Controller/ClinicController.js
@@ -110,16 +110,44 @@ module.exports = function (app, express) {
     });
     // get appointment of clinic
     apiRouter.get("/appointment", function (req, res) {
-        var username = req.query.username;
-        new db.Clinic({ "username": username })
-            .fetch({ withRelated: ["appointments"] })
-            .then(function (model) {
-                var clinic = model.toJSON();
-                res.json(utils.makeResponse(true, clinic, null));
-            })
-            .catch(function (err) {
-                console.log(err);
-            })
+        var clinicUsername = req.query.clinicUsername;
+        var sql = "SELECT * FROM tbl_appointment WHERE clinicUsername = '" + clinicUsername + "' AND DATE(appointmentTime) = CURRENT_DATE()";        
+        db.knex.raw(sql)
+        .then(function(collection){
+            result = collection[0];                
+            if(result.length > 0){  
+                var tmp = [];
+                for(var i in result){        
+                    tmp.push(result[i].patientID);
+                }
+                db.Patient.forge()
+                .where("patientID", "in", tmp)
+                .fetchAll()
+                .then(function(patientsResult){
+                    var results = [];
+                    for(var i in result){
+                        var tmpAppointment = JSON.parse(JSON.stringify(result[i]));
+                        // console.log(tmpAppointment);
+                        for(j in patientsResult.models){                                                        
+                            var tmpPatient = patientsResult.models[j].toJSON();                                                        
+                            if(tmpAppointment.patientID == tmpPatient.patientID){                            
+                                tmpAppointment.patient = tmpPatient;                                
+                            }
+                        }   
+                        delete tmpAppointment.clinicUsername;
+                        delete tmpAppointment.patientID;
+                        results.push(tmpAppointment);
+                    }
+                    res.json(utils.makeResponse(true, results, null));
+                });                
+            } else{
+                res.json(utils.makeResponse(false, null, "Không có cuộc hẹn nào"));
+            }                      
+        })
+        .catch(function(err){
+            var responseObj = utils.makeResponse(false, null, err.message);
+            res.json(responseObj);            
+        });
     });
     return apiRouter;
 }
