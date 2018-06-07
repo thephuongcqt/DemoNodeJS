6a5a90e1c51ccb0df2afbacdb4905e9f2d2748aa
DESKTOP-FND6BJ4\server_guest <dyncse61032@fpt.edu.vn>
2018-06-07 11:07:00 +0700

    no message

--- Utils/DBUtils.js
+++ Utils/DBUtils.js
@@ -1,13 +1,13 @@
 const knex = require('knex')({
   client: 'mysql',
   connection: {
-    // host: '27.74.245.84',
+    host: '27.74.245.84',
     // host: '203.205.29.13',
-    host: 'localhost',
+    // host: 'localhost',
     port: '3306',
     user: 'root',
-    // password: '123456',
-    password: '',
+    password: '123456',
+    // password: '',
     database: 'callcenter',
     charset: 'utf8'
   }
