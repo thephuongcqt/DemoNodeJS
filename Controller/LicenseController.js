38cc90f81947896e132386512e3ee50a85a9f011
duyncse61032 <duyncse61032@fpt.edu.vn>
2018-06-08 16:11:51 +0700

    fix get all license

--- Controller/LicenseController.js
+++ Controller/LicenseController.js
@@ -6,19 +6,18 @@ module.exports = function (app, express) {
     apiRouter = express.Router();
     // get All lincese
     apiRouter.get("/getAllLicense", function (req, res) {
-        new db.License()
+        db.License.forge()
             .fetchAll()
             .then(function (collection) {
-                if (collection = ! null) {
-                    res.json(utils.responseSuccess(collection.toJSON()));
+                if (collection == null) {
+                    res.json(utils.responseFailure(err.message));
                 } else {
-                    res.json(utils.responseFailure("License is not exist"));
+                    res.json(utils.responseSuccess(collection.toJSON()));
                 }
-
             })
             .catch(function (err) {
                 res.json(utils.responseFailure(err.message));
-            })
+            });
     });
     // create license
     apiRouter.post("/create", function (req, res) {
