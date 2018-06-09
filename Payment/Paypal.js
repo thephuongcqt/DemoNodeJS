996fa716866824da2f30c08b63e97d725b8b3149
Nguyen The Phuong <thephuongcqt@gmail.com>
2018-06-09 16:41:53 +0700

    checkout

--- Payment/Paypal.js
+++ Payment/Paypal.js
@@ -10,21 +10,49 @@ var gateway = braintree.connect({
     privateKey: "b305cabe62b01c1cc1d6c37b575139b8"
 });
 
+var gateway = braintree.connect({
+    environment:  braintree.Environment.Sandbox,
+    merchantId:   'pfg7tm6zrnm22cjc',
+    publicKey:    'f5b3g3dznrmgqwxj',
+    privateKey:   'b305cabe62b01c1cc1d6c37b575139b8'
+});
+
 
 
 module.exports = function (app, express) {
     var apiRouter = express.Router();
-    
-    apiRouter.get("/getToken", function(req, res){
-        gateway.clientToken.generate({}, function (err, response) {    
-            if(typeof response !== 'undefined'){                
+
+    apiRouter.get("/getToken", function (req, res) {
+        gateway.clientToken.generate({}, function (err, response) {
+            if (typeof response !== 'undefined') {
                 res.json(utils.responseSuccess(response));
-            } else{       
-                res.json(utils.responseFailure(err.message)); 
+            } else {
+                res.json(utils.responseFailure(err.message));
                 console.log(err);
-            }   
+            }
         });
     });
+
+    apiRouter.post("/checkout", function(req, res){
+        var username = req.body.username;
+        var licenseID = req.body.licenseID;
+        var nonce = req.body.nonce;
+        res.end();
+    });
+
+    // apiRouter.get("/getTokenForCustomer", function (req, res) {
+    //     var aCustomerId = "259682334";
+    //     gateway.clientToken.generate({
+    //         customerId: aCustomerId
+    //     }, function (err, response) {
+    //         if (typeof response !== 'undefined') {
+    //             res.json(utils.responseSuccess(response));
+    //         } else {
+    //             res.json(utils.responseFailure(err.message));
+    //             console.log(err);
+    //         }
+    //     });
+    // });
 
     return apiRouter;
 }
