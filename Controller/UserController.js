1f88dccaf8fcfd55adf1996b44ef01d2b53e3fa4
duyncse61032 <duyncse61032@fpt.edu.vn>
2018-06-19 15:57:14 +0700

    reset password and send email

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -2,6 +2,8 @@ var db = require("../DataAccess/DBUtils");
 var utils = require("../Utils/Utils");
 var Const = require("../Utils/Const");
 var logger = require("../Utils/Logger");
+var nodemailer = require('nodemailer');
+var Moment = require('moment');
 
 module.exports = function (app, express) {
     var apiRouter = express.Router();
@@ -116,6 +118,7 @@ module.exports = function (app, express) {
                         })
                         .catch(function (err) {
                             res.json(utils.responseFailure(err.message));
+                            logger.log(err.message, "createAdmin", "UserController");
                         });
                 } else {
                     res.json(utils.responseFailure("This username have been exist"));
@@ -123,6 +126,7 @@ module.exports = function (app, express) {
             })
             .catch(function (err) {
                 res.json(utils.responseFailure(err.message));
+                logger.log(err.message, "createAdmin", "UserController");
             });
     });
     //check duplicate username
@@ -160,6 +164,47 @@ module.exports = function (app, express) {
                 logger.log(err.message, "checkPassword", "UserController");
             });
     });
+
+    //---------------------------------------------------------------------------------------------//
+    function sendEmailToPatient(email, username, password) {
+        //  Send Email to patient when reset password successfull 
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'callcentercapstone@gmail.com',
+                pass: 'Callcenterpass1Callcenterpass1'
+            }
+        });
+        var currentDate = Moment(new Date()).format('DD-MM-YYYY');
+        var currentTime = Moment(new Date()).format('HH:mm:ss');
+        const mailOptions = {
+            from: 'callcentercapstone@gmail.com', // sender address
+            to: email, // list of receivers
+            subject: 'Thông tin khôi phục mật khẩu', // Subject line
+            html: '<!DOCTYPE html>' +
+                '<html><head><title>Appointment</title>' +
+                '</head><body><div style="padding:10px 250px 5px 250px;text-align:center">' +
+                '<img src="https://www.brandcrowd.com/gallery/brands/pictures/picture13882532337876.jpg" alt="logo" width="160" height="100">' +
+                '<tr><td style="background:#00b9f2;height:5px;line-height:5px;width:233px"></td>' +
+                '<td style="background:#f7941d;height:5px;line-height:5px;width:233px"></td>' +
+                '<td style="background:#5cb85c;height:5px;line-height:5px;width:233px"></td></tr></div>' +
+                '<div style="padding:5px 250px 10px 250px"><h1 style="color:#03a9f4;text-align:center">Quên mật khẩu?</h1>' +
+                '<h1 style="color:#03a9f4;text-align:center">Thiết lập mật khẩu mới!</h1>' +
+                '<h3>Xin chào,</h3>' +
+                '<p>Cảm ơn bạn đã yêu cầu đặt lại mật khẩu.</p>' +
+                '<p>Mật khẩu cho tài khoản ' + '<strong style="color:#15c; font-size:120%;">' + username + '</strong>' + ' đã được thay đổi thành công.</p>' +
+                '<p>Đây là mật khẩu mới của bạn: <strong style="color:#15c; font-size:120%;">' + password + '</strong></p>' +
+                '<p>Bạn yêu cầu đặt lại mật khẩu ngày ' + currentDate + ' lúc ' + currentTime + '.</p>' +
+                '</div></body></html>'
+        };
+        transporter.sendMail(mailOptions, function (err, info) {
+            if (err) {
+                res.json(utils.responseFailure(err.message));
+                logger.log(err.message, "sendEmailError", "UserController");
+            }
+        });
+    }
+    //---------------------------------------------------------------------------------------------//
     //reset password
     apiRouter.post("/resetPassword", function (req, res) {
         var username = req.body.username;
@@ -183,17 +228,18 @@ module.exports = function (app, express) {
                             .save({ "password": randomstring }, { patch: true })
                             .then(function (model) {
                                 res.json(utils.responseSuccess("Reset password successful"));
+                                sendEmailToPatient(email, username, randomstring);
                             })
                             .catch(function (err) {
                                 res.json(utils.responseFailure("Reset password fail"));
-                                logger.log(err.message, "checkPassword", "UserController");
+                                logger.log(err.message, "resetPassword", "UserController");
                             });
                     }
                 }
             })
             .catch(function (err) {
                 res.json(utils.responseFailure(err.message));
-                logger.log(err.message, "checkPassword", "UserController");
+                logger.log(err.message, "resetPassword", "UserController");
             });
     });
     // update information
