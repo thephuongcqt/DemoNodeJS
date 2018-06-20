c0a93df17311481ddbebd01e3514787c9f168b08
duyncse61032 <duyncse61032@fpt.edu.vn>
2018-06-20 12:52:31 +0700

    update

--- DataAccess/BaseDAO.js
+++ DataAccess/BaseDAO.js
@@ -48,7 +48,6 @@ var dao = {
         return new Promise((resolve, reject) => {
             var json = {};
             json[idName] = id;
-
             table.forge(json)
                 .fetch()
                 .then(model => {
