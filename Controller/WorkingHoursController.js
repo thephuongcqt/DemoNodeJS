1cc95328bc5852c26d38abf184a7bf2ad3932895
duyncse61032 <duyncse61032@fpt.edu.vn>
2018-06-24 13:39:40 +0700

    fix working hours

--- Controller/WorkingHoursController.js
+++ Controller/WorkingHoursController.js
@@ -20,44 +20,41 @@ module.exports = function (app, express) {
             });
     });
 
-    // update working hours with one apply date
-    apiRouter.post("/update", function (req, res) {
+    // update one working hours for apply dates
+    apiRouter.post("/update",async function (req, res) {
         var username = req.body.username;
         var startWorking = req.body.startWorking;
         var endWorking = req.body.endWorking;
-        var parseStartWorking = Moment(startWorking, "h:mm:ss A").format("HH:mm:ss");
-        var parseEndWorking = Moment(endWorking, "h:mm:ss A").format("HH:mm:ss");
+        var parseStartWorking = Moment(req.body.startWorking, "h:mm:ss A").format("HH:mm:ss");
+        var parseEndWorking = Moment(req.body.endWorking, "h:mm:ss A").format("HH:mm:ss");
         var applyDates = req.body.applyDate;
         var isDayOff = req.body.isDayOff;
-        getWorkingHours(username)
-            .then(function (results) {
-                if (applyDates == null) {
-                    res.json(utils.responseFailure("Vui lòng chọn ngày áp dụng"));
-                } else {
-                    for (var i in applyDates) {
-                        var applyDate = applyDates[i];
-                        if (!isNaN(applyDate)) {
-                            workingHoursDAO.updateWorkingHour(username, applyDate, parseStartWorking, parseEndWorking, isDayOff)
-                                .catch(function (err) {
-                                    res.json(utils.responseFailure(err));
-                                    logger.log(err);
-                                });
-                        }
+        var checkStartWorking = Moment(parseStartWorking, "HH:mm:ss").isValid();
+        var checkEndWorking = Moment(parseEndWorking, "HH:mm:ss").isValid();
+        if (checkStartWorking == false) {
+            parseStartWorking = undefined;
+        }
+        if (checkEndWorking == false) {
+            parseEndWorking = undefined;
+        }
+        try {
+            if (!applyDates) {
+                res.json(utils.responseFailure("Vui lòng chọn ngày áp dụng"));
+            } else {
+                for (var i in applyDates) {
+                    var applyDate = applyDates[i];
+                    if (!isNaN(applyDate)) {
+                        workingHoursDAO.updateWorkingHour(username, applyDate, parseStartWorking, parseEndWorking, isDayOff);
                     }
-                    getWorkingHours(username)
-                        .then(function (result) {
-                            res.json(utils.responseSuccess(result));
-                        })
-                        .catch(function (err) {
-                            res.json(utils.responseFailure(err));
-                            logger.log(err);
-                        });
                 }
-            })
-            .catch(function (err) {
-                res.json(utils.responseFailure(err));
-                logger.log(err);
-            });
+                var resultUpdate = await getWorkingHours(username);
+                res.json(utils.responseSuccess(resultUpdate));
+            }
+        }
+        catch (err) {
+            res.json(utils.responseFailure(err));
+            logger.log(err);
+        }
     });
     // update working hours with all apply date
     apiRouter.post("/updateAll", async function (req, res) {
@@ -66,38 +63,51 @@ module.exports = function (app, express) {
         // var listValue = [{
         //     "startWorking": "08:00:00 AM",
         //     "endWorking": "8:00:00 PM",
-        //     "applyDate": 1
+        //     "applyDate": "r1"
         // },
         // {
-        //     "startWorking": "09:00:00 AM",
-        //     "endWorking": "9:00:00 PM",
+        //     "startWorking": "AM",
+        //     "endWorking": "10:00:00 PM",
         //     "applyDate": 0
         // }];
-        if (listValue == null) {
-            res.json(utils.responseFailure("Vui lòng nhập giờ làm việc"));
-        } else {
-            if (listValue.length > 0) {
-                for (var i = 0; i < listValue.length; i++) {
-                    var applyDate = listValue[i].applyDate;
-                    var startWorking = listValue[i].startWorking;
-                    var parseStartWorking = Moment(startWorking, "h:mm:ss A").format("HH:mm:ss");
-                    var endWorking = listValue[i].endWorking;
-                    var parseEndWorking = Moment(endWorking, "h:mm:ss A").format("HH:mm:ss");
-                    workingHoursDAO.updateWorkingHours(username, applyDate, parseStartWorking, parseEndWorking)
-                        .catch(function (err) {
-                            res.json(utils.responseFailure(err));
-                            logger.log(err);
-                        });
+        try {
+            if (listValue == null) {
+                res.json(utils.responseFailure("Vui lòng nhập giờ làm việc"));
+            } else {
+                if (listValue.length > 0) {
+                    for (var i = 0; i < listValue.length; i++) {
+                        var applyDate = listValue[i].applyDate;
+                        if (isNaN(applyDate)) {
+                            logger.log(applyDate);
+                        } else {
+                            var startWorking = listValue[i].startWorking;
+                            var parseStartWorking = Moment(startWorking, "h:mm:ss A").format("HH:mm:ss");
+                            var checkStartWorking = Moment(parseStartWorking, "HH:mm:ss").isValid();
+                            if (checkStartWorking == false) {
+                                parseStartWorking = undefined;
+                            }
+                            var endWorking = listValue[i].endWorking;
+                            var parseEndWorking = Moment(endWorking, "h:mm:ss A").format("HH:mm:ss");
+                            var checkEndWorking = Moment(parseEndWorking, "HH:mm:ss").isValid();
+                            if (checkEndWorking == false) {
+                                parseEndWorking = undefined;
+                            }
+                            if (isNaN(applyDate)) {
+                                applyDate = undefined;
+                                parseEndWorking = undefined;
+                                parseStartWorking = undefined;
+                            }
+                            await workingHoursDAO.updateWorkingHours(username, applyDate, parseStartWorking, parseEndWorking);
+                        }
+                    }
                 }
+                var resultUpdate = await getWorkingHours(username);
+                res.json(utils.responseSuccess(resultUpdate));
             }
-            await getWorkingHours(username)
-                .then(function (result) {
-                    res.json(utils.responseSuccess(result));
-                })
-                .catch(function (err) {
-                    res.json(utils.responseFailure(err));
-                    logger.log(err);
-                });
+        }
+        catch (err) {
+            res.json(utils.responseFailure(err));
+            logger.log(err);
         }
     });
     return apiRouter;
