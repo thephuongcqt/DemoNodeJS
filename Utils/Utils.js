9cc5db2aefaa8727d5e0508b559d47181f51470a
Nguyen The Phuong <thephuongcqt@gmail.com>
2018-07-12 10:21:19 +0700

    trim

--- Utils/Utils.js
+++ Utils/Utils.js
@@ -62,7 +62,7 @@ var utils = {
         for (var i = 0; i < splitStr.length; i++) {
             splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
         }
-        return splitStr.join(' ').replace(/\s\s+/g, ' ');;
+        return splitStr.join(' ').replace(/\s\s+/g, ' ').trim();;
     },
 
     checkValidateMessage: function (message) {
