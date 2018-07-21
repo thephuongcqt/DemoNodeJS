6d89179bb6c43743e21198bca0d0264c2bd71359
Nguyen The Phuong <thephuongcqt@gmail.com>
2018-07-21 17:36:32 +0700

    update get Regimen

--- Controller/RegimenController.js
+++ Controller/RegimenController.js
@@ -11,47 +11,45 @@ module.exports = function (app, express) {
     apiRouter.post("/getRegimen", async function (req, res) {
         try {
             var username = req.body.username;
-            var diseaseID = req.body.diseaseID;    
-            if(username && diseaseID){
-                var json = {
-                    clinicUsername: username,
-                    diseaseID: diseaseID
-                }
-                var regimens = await baseDAO.findByProperties(db.Regimen, json);        
-                if(regimens && regimens.length > 0){
-                    var reminding = regimens[0].reminding;
-                    
-                    var regimenMedicine = await baseDAO.findByPropertiesWithRelated(db.RegimenMedicine, json, "medicine");
-                    var medicines = [];
-                    for(var index in regimenMedicine){
-                        var item = regimenMedicine[index];
-                        var medicine = {
-                            "medicineID": item.medicineID,
-                            "quantity": item.quantity,
-                            "description": item.description,
-                            "unitName": item.medicine.unitName
+            var diseaseIDs = req.body.diseaseIDs;
+            if (username && diseaseIDs) {
+                var list = [];
+                for (var i in diseaseIDs) {
+                    var diseaseID = diseaseIDs[i];
+                    var json = {
+                        clinicUsername: username,
+                        diseaseID: diseaseID
+                    }
+                    var regimens = await baseDAO.findByProperties(db.Regimen, json);
+                    if (regimens && regimens.length > 0) {
+                        var reminding = regimens[0].reminding;
+                        var regimenMedicine = await baseDAO.findByPropertiesWithRelated(db.RegimenMedicine, json, "medicine");
+                        var medicines = [];
+                        for (var index in regimenMedicine) {
+                            var item = regimenMedicine[index];
+                            var medicine = {
+                                "medicineID": item.medicineID,
+                                "quantity": item.quantity,
+                                "description": item.description,
+                                "unitName": item.medicine.unitName
+                            }
+                            medicines.push(medicine);
                         }
-                        medicines.push(medicine);
+                        var jsonResponse = {
+                            reminding: reminding,
+                            medicines: medicines
+                        }
+                        list.push(jsonResponse);
                     }
-                    var jsonResponse = {
-                        reminding: reminding,
-                        medicines: medicines
-                    }
-                    res.json(utils.responseSuccess(jsonResponse));
-                    return;
-                } else{
-                    res.json(utils.responseSuccess({
-                        reminding: "",
-                        medicines: []
-                    }));
                 }
-            } else{
+                res.json(utils.responseSuccess(list));
+            } else {
                 res.json(utils.responseFailure("Đã có lỗi xảy ra khi lấy phác đồ điều trị"))
             }
         } catch (error) {
             logger.log(error);
             res.json(utils.responseFailure("Đã có lỗi xảy ra khi lấy phác đồ điều trị"))
-        }        
+        }
     });
 
     return apiRouter;
